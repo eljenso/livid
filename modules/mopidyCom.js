1b1c835ed389f6e84db947081be1e0ac9e1e3d3f
Jens Böttcher <eljenso.boettcher@gmail.com>
2015-05-05 19:34:08 +0200

    adding first song to tracklist

--- modules/mopidyCom.js
+++ modules/mopidyCom.js
@@ -2,7 +2,9 @@ var Q = require('q'),
     Mopidy = require('mopidy'),
     config = require('../config.js');
 
-var playlistTracks, mopidy;
+var playlistTracks;
+var userTracks = [];
+var mopidy;
 
 function init () {
   mopidy = Mopidy({
@@ -12,15 +14,40 @@ function init () {
   });
 
   mopidy.on("state:online", function () {
+
       getPlaylistTracks(config.mopidy.defaultPlaylist)
         .then(function (tracks) {
+          // Save fallback playlist
           playlistTracks = tracks;
+          // Clear current playlist
+          return mopidy.tracklist.clear();
+        })
+        .then(function () {
+          // Add next track to tracklist
+          return playNextTrack();
+        })
+        .then(function () {
+          // Start playing music
+          mopidy.playback.play();
         })
         .done();
-      mopidy.playback.next();
   });
 
   mopidy.connect();
+}
+
+
+function playNextTrack () {
+  var nextTrack;
+
+  // If no user tracks have been selected (yet), play tracks from fallback playlist
+  if (userTracks.length > 0) {
+    nextTrack = userTracks.shift();
+  } else {
+    nextTrack = playlistTracks.shift();
+  };
+
+  return mopidy.tracklist.add(null, null, nextTrack.uri, null);
 }
 
 function getPlaylistTracks (playlistName) {
