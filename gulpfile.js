b857334c645681b7d23c521cfef6373848a3f8f0
Jens Böttcher <eljenso.boettcher@gmail.com>
2015-05-05 17:44:51 +0200

    rerouting stout and sterr into text files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
     livereload = require('gulp-livereload'),
     nodemon = require('gulp-nodemon');
+var fs = require('fs');
 
 
 
@@ -9,12 +10,12 @@ gulp.task('default', function () {
   
   nodemon({
     script: 'server.js',
-    stdout: true,
-    ext: 'js jade',
-    // execMap: {
-    //   js: 'node-debug'
-    // }
+    stdout: false,
+    ext: 'js jade'
   }).on('readable', function() {
+    this.stdout.pipe(fs.createWriteStream('output.txt'));
+    this.stderr.pipe(fs.createWriteStream('err.txt'));
+    
     livereload.reload();
   }
 )})
