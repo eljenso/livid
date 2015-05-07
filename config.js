0e5108e5d056b05031efaa9066cda555728ed3e4
Jens Böttcher <eljenso.boettcher@gmail.com>
2015-05-07 22:24:40 +0200

    changed to standard mopidy ws port

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ var config = {
     port: process.env.PORT || 3000
   },
   mopidy:{
-    adress: 'ws://127.0.0.1:6681/mopidy/ws/',
+    adress: 'ws://127.0.0.1:6680/mopidy/ws/',
     defaultPlaylist: 'Einweihung'
   },
   db: 'mongodb://localhost/parti-q'
